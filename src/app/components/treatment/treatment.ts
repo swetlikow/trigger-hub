959fda6ab642405c33a22f4a97cb2dc21efb14d9
andrew svetlikov <andrewsvetlikov@gmail.com>
2025-11-27 12:29:09 +0100

    changed coloring

--- src/app/components/treatment/treatment.ts
+++ src/app/components/treatment/treatment.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-treatment',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './treatment.html',
   styleUrl: './treatment.css',
 })
